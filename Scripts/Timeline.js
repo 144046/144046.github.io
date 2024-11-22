23a30057d07e7f25a9c64fd1c537f1ea770483dc
Celeste <celestestrazzi@outlook.com>
2024-11-22 02:39:57 +0100

    ffesfs

--- Scripts/Timeline.js
+++ Scripts/Timeline.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  
+  //dislessia
 	var getTextNodesIn = function(el) { // Look at all the page elements and returns the text nodes
 	    return $(el).find(":not(iframe,script)").addBack().contents().filter(function() {
 	        return this.nodeType == 3; // Text node types are type 3
@@ -91,6 +91,77 @@ $(function(){
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
+//size randomizer
 
-	setInterval(messUpWords, 40);
+	function size() {
+	document.getElementById(getRandomInt(0,155)).style.fontSize = getRandomInt(17,23)+"px";
+}
+
+//bigscare
+
+var x = getRandomInt(0,155);
+
+	function bigScare(){
+		
+		document.getElementById(x).style.fontSize = getRandomInt(50,200)+"px";
+		setTimeout(reset, 500);
+	}
+
+function reset(){
+	document.getElementById(x).style.fontSize = getRandomInt(17,23)+"px";
+}
+
+
+//colors
+
+const colors = [
+  '#003300',
+  '#000033',
+  '#330000',
+  '#003333',
+  '#333333',
+  '#330033',
+  '#333300',
+  '#660000',
+  '#000066',
+  '#006600',
+  '#333366',
+  '#330066',
+  '#006633',
+  '#003366',
+  '#003366',
+];
+
+function color()
+{
+	document.getElementById(getRandomInt(0,155)).style.color = colors[getRandomInt(0,14)] ;
+}
+
+//corrompere ulteriormente
+
+function corrompo ()
+{
+	document.getElementById(getRandomInt(0,155)).innerHTML = "c̵̬͒'̶͕̤͋ ̸̹̂̊y̷͓̹͋̕m̸̢̬͐͆g̸̙̣̕'̵̦̊́ͅ ̵̺͈̿m̶̠͘g̶̻̭͒ȑ̶̲̚'̷͈̩̅̐ḻ̵͂͌ṳ̶̃̍h̸̫͊ ";
+}
+
+function traduco ()
+{
+	var x = getRandomInt(0,155);
+	if (document.getElementById(x).innerHTML == "c̵̬͒'̶͕̤͋ ̸̹̂̊y̷͓̹͋̕m̸̢̬͐͆g̸̙̣̕'̵̦̊́ͅ ̵̺͈̿m̶̠͘g̶̻̭͒ȑ̶̲̚'̷͈̩̅̐ḻ̵͂͌ṳ̶̃̍h̸̫͊ ")
+	{
+		document.getElementById(x).innerHTML = "WE SEE YOU ";
+		document.getElementById(x).style.textDecoration= "line-through";
+	}
+}
+
+
+
+
+//executions
+setInterval(traduco, 3000);
+setInterval(corrompo, 1000);
+setInterval(bigScare, 7000);
+setInterval(size, 50);
+setInterval(messUpWords, 150);
+setInterval(color, 50);
 });
